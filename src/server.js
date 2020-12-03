c3aafd17c4b03e76e7470bec2952dd284b08272f
eliezerladeira <eliezerladeira@yahoo.com.br>
2020-12-03 00:26:16 -0300

    Organização dos arquivos
    Instalação do pacote hbs

--- src/server.js
+++ src/server.js
@@ -5,13 +5,18 @@ const path = require ('path');
 // iniciando o express (starting express)
 const server = express();
 server
-// utilizando os arquivos estáticos (using static files)
-.use(express.static('public'))
+    // utilizando os arquivos estáticos (using static files)
+    .use(express.static('public'))
 
-// cria uma rota (create a route)
-server.get('/', (request, response) => {
-    return response.sendFile(path.join(__dirname, 'views', 'index.html'))
-})
+    // configurar template engine (set up template engine)
+    .set('views', path.join(__dirname, "views"))
+    .set('view engine', 'hbs')
+
+    // cria uma rota (create a route)
+    server.get('/', (request, response) => {
+        //return response.sendFile(path.join(__dirname, 'views', 'index.html'))
+        return response.render('index')
+    })
 
 // sobe o servidor (go up the server)
 server.listen(5500)
